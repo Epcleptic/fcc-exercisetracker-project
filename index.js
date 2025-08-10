9aff423ccdc85b9528127aac939fa04f3b1c9784
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2025-08-10 11:52:43 +0200

    Create a New User

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+// User(username)
+// Exercise(:_id, description, duration, date)
 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+});
 
+const User = mongoose.model("User", userSchema);
 
+const findUser = async (querry) => {
+  const result = await User.findOne(querry, (err, data) => {
+    if (err) return console.error(err);
+    return data;
+  });
+  return result;
+};
+
+const saveUser = async (data) => {
+  let user = await findUser({ username: data });
+  if (user) {
+    return user;
+  } else {
+    user = await User.create({ username: data });
+    return user;
+  }
+};
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/api/users", async (req, res) => {
+  const user = await saveUser(req.body.username);
+  if (user) {
+    res.json({
+      username: user.username,
+      _id: user._id,
+    });
+  } else {
+    res.json({
+      error: "Invalid User",
+    });
+  }
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
